16936ac91013f9c7f849fd9010bd6ee1f20e5490
infected-by-js <infectedbyjs@gmail.com>
2022-01-12 23:58:50 +1000

    setup models

---
+++ server/models/Product.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema(
+	{
+		title: { type: String, required: true, unique: true },
+		author: { type: String, required: true },
+		style: { type: String, required: true },
+		size: { type: String, required: true },
+		description: { type: String, required: true },
+		price: { type: Number, required: true },
+		category: { type: String, required: true },
+		year_created: { type: Number, required: true },
+		image_set: { type: Array, required: true },
+	},
+	{
+		timestamps: true,
+	}
+);
+module.exports = mongoose.model('Product', ProductSchema);
