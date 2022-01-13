19121b8697c79708bbedaa5a4d97ec37fde322a8
infected-by-js <infectedbyjs@gmail.com>
2022-01-13 23:35:20 +1000

    add jwt. add update user by id

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const { verifyToken, verifyTokenAndAuth } = require('./verifyToken');
+
+router.put('/:id', verifyTokenAndAuth, async (req, res) => {
+	if (req.body.password) {
+		req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+	}
+
+	try {
+		const updateUser = await User.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: req.body,
+			},
+			{ new: true }
+		);
+
+		res.status(200).json(updateUser);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
 module.exports = router;
