f65ccb52bd2b3934cd23c3f8fcecd83b69bf1cf5
infected-by-js <infectedbyjs@gmail.com>
2022-01-13 23:34:38 +1000

    add auth route(register & login)

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
-
 const PORT = process.env.PORT || 3000;
 
 mongoose
@@ -14,6 +15,7 @@ mongoose
 	.catch((err) => console.log(err));
 
 app.use(express.json());
+app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 
 app.listen(PORT, () => {
