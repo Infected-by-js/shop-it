e3a3343aa2bd296a652b9e5fd4da7068512b3fdf
infected-by-js <infectedbyjs@gmail.com>
2022-01-16 02:31:00 +1000

    add payment route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(PORT, () => {
 	console.log('Server in running!');
